2742af3364b4d5197d0a3ec6e04de0cecdee66c6
tarako25 <takenoko1603@gmail.com>
2023-05-24 01:09:44 +0900

    env化

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ app.listen(process.env.PORT || 3000, () => {
 //Mysql設定
 
 const mysql_setting = {
-  host: "database-1.c26tz3blblk0.ap-northeast-1.rds.amazonaws.com",
-  port:"3306",
-  user:"admin",
-  password:"0423take",
-  database: "stop-app-db",
+  host: process.env.HOST,
+  port:process.env.PORT,
+  user:process.env.USER,
+  password:process.env.PASSWORD,
+  database: process.env.DATABASE,
 };
 const mysql_setting2 = {
-  host: "database-1.c26tz3blblk0.ap-northeast-1.rds.amazonaws.com",
-  port:"3306",
-  user:"admin",
-  password:"0423take",
-  database: "stop-app-acount",
+  host: process.env.HOST,
+  port:process.env.PORT,
+  user:process.env.USER,
+  password:process.env.PASSWORD,
+  database: process.env.DATABASE2,
 };
 
 //グローバルID
